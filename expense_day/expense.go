7d9a81bb043c30817b266344d06fc6b92e9fc40d
Karan Sivrat <bomb0069@gmail.com>
2018-09-25 01:42:03 +0700

    add feature : add one day with food 20

---
+++ expense_day/expense.go
@@ -0,0 +1,58 @@
+package expense_day
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var dayMapper = map[string]string{
+	"5": "Mon",
+	"6": "Tue",
+	"7": "Wed",
+	"1": "Thu",
+	"2": "Fri",
+	"3": "Sat",
+	"4": "Sun",
+}
+
+type Expense struct {
+	byDay map[string]int
+}
+
+func NewExpense() Expense {
+	byDay := map[string]int{
+		"Mon": 0,
+		"Tue": 0,
+		"Wed": 0,
+		"Thu": 0,
+		"Fri": 0,
+		"Sat": 0,
+		"Sun": 0,
+	}
+	return Expense{byDay}
+}
+
+func (e *Expense) read(fileName string) {
+	data, _ := ioutil.ReadFile(fileName)
+	splittedLine := strings.Split(string(data), "\r\n")
+	for _, line := range splittedLine {
+		expenseDate := strings.Fields(line)
+		day := ""
+		for index, word := range expenseDate {
+			expenseCostString := ""
+			if index == 0 {
+				day = dayMapper[word]
+			} else {
+				expenseCostString = word[1:len(word)]
+				expenseCost, _ := strconv.Atoi(expenseCostString)
+				e.byDay[day] = expenseCost
+			}
+		}
+
+	}
+}
+
+func (e *Expense) summaryByDay() map[string]int {
+	return e.byDay
+}
--- expense_day/expense_day_test.go
+++ expense_day/expense_day_test.go
@@ -26,3 +26,26 @@ func Test_empty_file_will_return_all_0(t *testing.T) {
 		t.Errorf("%v != %v", expected, actual)
 	}
 }
+
+func Test_expense_first_with_20_food(t *testing.T) {
+	expected := map[string]int{
+		"Mon": 0,
+		"Tue": 0,
+		"Wed": 0,
+		"Thu": 20,
+		"Fri": 0,
+		"Sat": 0,
+		"Sun": 0,
+	}
+
+	expense := NewExpense()
+
+	expense.read("expense_first_with_20_food.txt")
+
+	actual := expense.summaryByDay()
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("%v != %v", expected, actual)
+	}
+
+}
