70dbcc49738c76ab8f21381c596c9b69cb5ac0d1
Karan Sivrat <bomb0069@gmail.com>
2018-09-25 01:58:36 +0700

    add feature : add first week for expense

--- expense_day/expense_day_test.go
+++ expense_day/expense_day_test.go
@@ -50,7 +50,7 @@ func Test_expense_first_with_20_food(t *testing.T) {
 
 }
 
-func Test_full_opening_with_(t *testing.T) {
+func Test_first_day(t *testing.T) {
 	expected := map[string]int{
 		"Mon": 0,
 		"Tue": 0,
@@ -64,6 +64,29 @@ func Test_full_opening_with_(t *testing.T) {
 	expense := NewExpense()
 
 	expense.read("expense_first_day.txt")
+
+	actual := expense.summaryByDay()
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("%v != %v", expected, actual)
+	}
+
+}
+
+func Test_first_week(t *testing.T) {
+	expected := map[string]int{
+		"Mon": 541,
+		"Tue": 237,
+		"Wed": 62,
+		"Thu": 227,
+		"Fri": 453,
+		"Sat": 20,
+		"Sun": 54,
+	}
+
+	expense := NewExpense()
+
+	expense.read("expense_first_week.txt")
 
 	actual := expense.summaryByDay()
 
